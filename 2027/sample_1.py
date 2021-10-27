608c78137d2841e7e3ccfec6e332e7caf2640a43
Souken Niimi <213kyoudaisouken@gmail.com>
2021-10-27 10:15:33 +0900

    sample_1

---
+++ 2027/sample_1.py
@@ -0,0 +1,12 @@
+while True:
+    num = int(input("整数を入力してください(終了 -> 0)"))
+    if num == 0:
+        break
+    elif num % 2 == 0:
+        {
+            print('偶数です')
+        }
+    else:
+        {
+            print('奇数です')
+        }
